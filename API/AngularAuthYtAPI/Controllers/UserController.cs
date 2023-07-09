ef557c6b9a9bac643ea3a429c68ba24d79f2e979
Okba HAMDI <94719238+OkbaHamdi@users.noreply.github.com>
2023-07-09 19:43:51 +0100

    Add files via upload

---
+++ API/AngularAuthYtAPI/Controllers/UserController.cs
@@ -0,0 +1,203 @@
+﻿using AngularAuthYtAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System.Text;
+using AngularAuthYtAPI.Context;
+using Microsoft.EntityFrameworkCore;
+using AngularAuthYtAPI.Helpers;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using AngularAuthYtAPI.Models.Dto;
+
+namespace AngularAuthYtAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext context)
+        {
+            _authContext = context;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username && x.Password==userObj.Password);
+
+            if (user == null)
+                return NotFound(new { Message = "User incorrect" });
+
+            // check role
+            /*if (user.Role != "ADMIN" && user.Role != "ADMIN BANK")
+            {
+                return Ok(new
+                {
+                    Message = "User is not an Admin "
+                }
+                );
+            }*/
+            user.Token = CreateJwt(user);
+            return Ok(new
+           {
+               Token= user.Token,
+               Message="Login Success! "
+           }
+            );
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> AddUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            // check email
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email Already Exist" });
+
+            //check username
+            if (await CheckUsernameExistAsync(userObj.Username))
+                return BadRequest(new { Message = "Username Already Exist" });
+            
+
+            // check organisation
+            var user = await _authContext.BankInfos
+                .FirstOrDefaultAsync(x => x.Organisation == userObj.Organisation);
+            if(user == null)
+            {
+                return BadRequest(new { Message = "Organisation Not found" });
+                
+            }
+
+            userObj.Bankcode= user.Bankcode;
+            userObj.Lockout_enable = "0";
+            await _authContext.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                //Status = 200,
+                Message = "User Added!"
+            });
+        }
+
+        private Task<bool> CheckEmailExistAsync(string? email)
+            => _authContext.Users.AnyAsync(x => x.Email == email);
+
+        private Task<bool> CheckUsernameExistAsync(string? username)
+            => _authContext.Users.AnyAsync(x => x.Email == username);
+
+        private static string CheckPasswordStrength(string pass)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (pass.Length < 9)
+                sb.Append("Minimum password length should be 8" + Environment.NewLine);
+            if (!(Regex.IsMatch(pass, "[a-z]") && Regex.IsMatch(pass, "[A-Z]") && Regex.IsMatch(pass, "[0-9]")))
+                sb.Append("Password should be AlphaNumeric" + Environment.NewLine);
+            if (!Regex.IsMatch(pass, "[<,>,@,!,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))
+                sb.Append("Password should contain special charcter" + Environment.NewLine);
+            return sb.ToString();
+        }
+
+        private string CreateJwt(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Name,$"{user.Username}")
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        /*private string CreateRefreshToken()
+        {
+            var tokenBytes = RandomNumberGenerator.GetBytes(64);
+            var refreshToken = Convert.ToBase64String(tokenBytes);
+
+            var tokenInUser = _authContext.Users
+                .Any(a => a.RefreshToken == refreshToken);
+            if (tokenInUser)
+            {
+                return CreateRefreshToken();
+            }
+            return refreshToken;
+        }
+
+        private ClaimsPrincipal GetPrincipleFromExpiredToken(string token)
+        {
+            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = false
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token,tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("This is Invalid Token");
+            return principal;
+                
+        }
+        */
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            return Ok(await _authContext.Users.ToListAsync());
+        }
+
+        /*[HttpPost("refresh")]
+        public async Task<IActionResult> Refresh([FromBody]TokenApiDto tokenApiDto)
+        {
+            if (tokenApiDto is null)
+                return BadRequest("Invalid Client Request");
+            string accessToken = tokenApiDto.AccessToken;
+            string refreshToken = tokenApiDto.RefreshToken;
+            var principal = GetPrincipleFromExpiredToken(accessToken);
+            var username = principal.Identity.Name;
+            var user = await _authContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+            if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+                return BadRequest("Invalid Request");
+            var newAccessToken = CreateJwt(user);
+            var newRefreshToken = CreateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            await _authContext.SaveChangesAsync();
+            return Ok(new TokenApiDto()
+            {
+                AccessToken = newAccessToken,
+                RefreshToken = newRefreshToken,
+            });
+        }*/
+    }
+
+}
